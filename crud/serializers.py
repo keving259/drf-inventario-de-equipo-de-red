22b9cda51fb61b79ad3cdbe08651252684bfbb6f
kcisner <kevingael.cisneros047@gmail.com>
2026-08-28 11:50:06 -0600

    Add serializers for Dispositivo and Puerto models

---
+++ crud/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Dispositivo, Puerto
+
+class DispositivoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Dispositivo
+        fields = ('nombre', 'tipo', 'marca', 'modelo', 'ip_gestion', 'ubicacion', 'propietario')
+
+class PuertoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Puerto
+        fields = ('dispositivo', 'numero_puerto', 'tipo', 'velocidad', 'estado', 'conectado_a')
